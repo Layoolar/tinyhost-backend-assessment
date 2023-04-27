4f4026e0dd4b6bbf3b5f3e784db96ee0149fb32a
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-04-27 01:25:08 +0100

    Basic working upload api done

--- app/aws/upload.js
+++ app/aws/upload.js
@@ -1,17 +1,43 @@
+const AdmZip = require('adm-zip');
+
 const upload = async (req, res) => {
   try {
-    // TODO: Complete code here
+    const email = req.body.email;
+    const file = req.file;
 
-    return res
-      .status(200)
-      .json({
-        fileCount: 0,
-        largestFile: '',
-        email: ''
-      })
-  } catch (err) {
-    res.sendStatus(500)
+    // Check if both email and file are present
+    if (!email || !file) {
+      return res.status(400).json({ error: 'Both email and file are required' });
+    }
+    // Extract zip file
+    const zip = new AdmZip(file.buffer);
+    const zipEntries = zip.getEntries();
+
+    // Get file count and largest file
+    let fileCount = 0;
+    let largestFileSize = 0;
+    let largestFileName = '';
+
+    zipEntries.forEach((entry) => {
+      if (!entry.isDirectory && !entry.name.startsWith('.')) {
+        fileCount++;
+        if (entry.header && entry.header.size > largestFileSize) {
+          largestFileSize = entry.header.size;
+          largestFileName = entry.name;
+        }
+      }
+    });
+
+    res.status(200).json({
+      email: email,
+      fileCount: fileCount,
+      largestFileSize: `${largestFileSize} bytes`,
+      largestFileName: largestFileName
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to process the zip file' });
   }
-}
+};
 
 export default upload;
